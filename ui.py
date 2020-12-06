dfd73e5b77c14aa8b281da82bf24abfa41841398
Steven Steele <steeletet@gmail.com>
2020-12-06 01:37:25 -0800

    added DFS

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
-from solvemaze import solveMaze 
+from solvemaze import solveMazeBFS, solveMazeDFS
 
 def chooseFile():
     window.filename = filedialog.askopenfilename(initialdir = "/", title = "Choose a file", filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
@@ -18,7 +18,10 @@ pickMaze.pack(fill="x")
 BFS = tk.Button(window, text="BFS User Maze", command=lambda : solveMaze(window.filename))
 BFS.pack(fill="x")
 
-BFSdefault = tk.Button(window, text="BFS Hard Maze", command=lambda : solveMaze("maze_hard.png"))
+BFSdefault = tk.Button(window, text="BFS Hard Maze", command=lambda : solveMazeBFS("maze_hard.png"))
+BFSdefault.pack(fill="x")
+
+BFSdefault = tk.Button(window, text="DFS Hard Maze", command=lambda : solveMazeDFS("maze_hard.png"))
 BFSdefault.pack(fill="x")
 
 BFSez = tk.Button(window, text="BFS Easy Maze", command=lambda : solveMaze("fish.jpg"))
